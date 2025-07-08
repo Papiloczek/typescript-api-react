59ea38e57424557c30ce42dd9118808026bcfb6d
HPAVILION15BLUE\Linka <papiptok@gmail.com>
2025-07-08 11:18:18 +0200

    serwer, express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>serwer</h1>');
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//   res.end('<h1>serwer</h1>');
+// });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Serwer działa na http://localhost:${PORT}`);
-});
+// const PORT = 3060;
+// server.listen(PORT, () => {
+//   console.log(`Serwer działa na http://localhost:${PORT}`);
+// });
